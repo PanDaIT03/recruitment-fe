05ea5c7e649108c99ff798c5515b30be7dbdc1dc
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-08-30 08:56:46 +0700

    [FIX] change folder

--- src/config/routesConfig.tsx
+++ src/config/routesConfig.tsx
@@ -1,11 +1,11 @@
 import { RouteObject } from 'react-router-dom';
 import Home from '~/pages/Home/Home';
 import NotFound from '~/pages/NotFound/NotFound';
+import adminRoutes from '~/routes/adminRoutes';
+import employerRoutes from '~/routes/employerRoutes';
 import PublicRoute from '~/routes/PublicRoute';
+import userRoutes from '~/routes/userRoutes';
 import PATH from '~/utils/path';
-import adminRoutes from './adminRoutes';
-import employerRoutes from './employerRoutes';
-import userRoutes from './userRoutes';
 
 const routesConfig: RouteObject[] = [
   {
