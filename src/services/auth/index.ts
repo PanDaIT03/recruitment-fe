d90e80a0c1db51e124a14a63952ad5885ba3e954
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-09-11 13:58:23 +0700

    [FIX] test api auth

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,15 +1,7 @@
 import axiosApi from '~/services/axios';
 import { IBaseUser } from '~/types/Auth';
 
-const apiSignUp = async (data: IBaseUser) => {
-  // try {
-  //   const response = await axiosApi.post('auth/register', data);
-  //   return response;
-  // } catch (error) {
-  //   console.error('Error in apiSignUp:', error);
-  //   throw error;
-  // }
-  console.log(data);
-};
+const apiSignUp = (data: IBaseUser) => console.log(data);
+// axiosApi.post('auth/register', data);
 
 export default apiSignUp;
