cc3c4e25d8850bb5f0115fab4101edb1ff380c50
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-09-14 09:48:12 +0700

    [FEAT] middle ware save state redux to local

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+  Middleware,
+} from '@reduxjs/toolkit';
 import { authReducer } from './reducer/auth';
+import {
+  loadState,
+  localStorageMiddleware,
+} from './middlewares/localStorageMiddleware';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const preloadedState = loadState();
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
+  reducer: rootReducer,
+  preloadedState,
+  middleware: (getDefaultMiddleware) => {
+    const defaultMiddleware = getDefaultMiddleware();
+    return defaultMiddleware.concat(localStorageMiddleware as Middleware);
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
