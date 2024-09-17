5a617a6344114c9c1e96a011b2421f3e06f0ff37
Duong Dai <daiphucduongvinh203@gmail.com>
2024-09-17 22:18:32 +0700

    [FIX] fix first animation when render is not applied

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -13,36 +13,37 @@ type IProps = {
   animationType?: AnimationType;
 } & ModalProps;
 
+const customAnimation: CustomAnimation = {
+  'slide-down': {
+    maskTransitionName: 'custom-mask',
+    transitionName: 'custom-modal-slide-down',
+  },
+  default: {},
+};
+
 const Modal = ({
   isOpen,
   children,
   animationType = 'default',
   ...props
 }: IProps) => {
+  const { transitionName } = customAnimation[animationType];
+
+  const combinedProps = {
+    ...props,
+    ...customAnimation[animationType],
+  };
+
   const customClassNames: ModalProps['classNames'] = {
-    content: '!p-0',
+    content: `!p-0 ${transitionName}-${isOpen ? 'enter' : 'leave'}`, // This ensures the Modal has an opening animation even on first render
     header:
       '!mb-0 !px-[24px] !pt-[20px] !pb-[8px] !border-b-[1px] !border-solid !border-[#e5e7eb]',
     body: '!px-[24px] !pt-[10px] !pb-[20px]',
     footer: '!mt-0 !px-[24px] !py-[20px] !rounded-b-[8px]',
   };
 
-  const customAnimation: CustomAnimation = {
-    'slide-down': {
-      maskTransitionName: 'custom-mask',
-      transitionName: 'custom-modal-slide-down',
-    },
-    default: {},
-  };
-
   return (
-    <ModalAntd
-      centered
-      open={isOpen}
-      classNames={customClassNames}
-      {...customAnimation[animationType]}
-      {...props}
-    >
+    <ModalAntd open={isOpen} classNames={customClassNames} {...combinedProps}>
       {children}
     </ModalAntd>
   );
