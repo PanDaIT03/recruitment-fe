61087dbca0f287af6fb21f70a6d9721b71bcd91d
Duong Dai <daiphucduongvinh203@gmail.com>
2024-09-23 10:42:34 +0700

    add icon svg

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,2 +1,3 @@
 export { ReactComponent as HeaderLogo } from './logo-text-mix-white.svg';
 export { ReactComponent as UserAvatar } from './user_avatar.svg';
+export { ReactComponent as PortFolio } from './portfolio.svg';
