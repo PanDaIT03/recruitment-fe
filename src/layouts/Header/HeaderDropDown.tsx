cc059c7c9245f0fb1f0b6e7add9d7f2859b108ba
Duong Dai <daiphucduongvinh203@gmail.com>
2024-09-24 13:50:57 +0700

    [FIX] add googleLogout func

--- src/layouts/Header/HeaderDropDown.tsx
+++ src/layouts/Header/HeaderDropDown.tsx
@@ -1,3 +1,4 @@
+import { googleLogout } from '@react-oauth/google';
 import { Dropdown, Image, MenuProps } from 'antd';
 import { memo, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -31,6 +32,8 @@ const HeaderDropDown = () => {
   };
 
   const handleOkModal = () => {
+    googleLogout();
+
     dispatch(signOut()).then(() => {
       setIsOpen(false);
       navigate(PATH.SIGN_IN);
