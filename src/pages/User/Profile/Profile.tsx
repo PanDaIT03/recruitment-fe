2adb5ea4b6152852a17b58c2a2f41fa37a47d4b6
PanDaIT03 <daiphucduongvinh203@gmail.com>
2024-09-25 22:17:13 +0700

    [FEAT] add user profile page

--- src/pages/User/Profile/Profile.tsx
+++ src/pages/User/Profile/Profile.tsx
@@ -1,23 +1,98 @@
-import { Tooltip } from 'antd';
-import Title from 'antd/es/typography/Title';
-import React from 'react';
+import { Divider } from 'antd';
+import { useState } from 'react';
+import { Achievement, Bag, Language, PencilSkill } from '~/assets/svg';
+
 import ButtonAction from '~/components/Button/ButtonAction';
+import ProfileSection, {
+  IProfileSection,
+} from '~/components/ProfileSection/ProfileSection';
 import icons from '~/utils/icons';
 
-const { EditOutlined } = icons;
+const { PlusOutlined, EditOutlined } = icons;
 
-const Profile: React.FC = () => {
+const Profile = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleClick = () => {
+    setIsOpenModal(true);
+  };
+
+  const items: IProfileSection[] = [
+    {
+      header: {
+        title: 'Thành tích / Kỹ năng nổi bật',
+        suffixIcon: (
+          <Achievement width={20} height={20} className="font-bold" />
+        ),
+      },
+      buttonTitle: 'Cập nhật tóm tắt',
+      content:
+        'Tóm tắt về thành tích / kỹ năng nổi bật giúp hồ sơ của bạn tăng 3.9 lần lượt tiếp cận từ nhà tuyển dụng.',
+      buttonAction: (
+        <ButtonAction
+          title={<EditOutlined className="text-[#691f74] cursor-pointer" />}
+          onClick={handleClick}
+        />
+      ),
+    },
+    {
+      header: {
+        title: 'Kinh nghiệm',
+        suffixIcon: <Bag width={20} height={20} className="font-bold" />,
+      },
+      buttonTitle: 'Thêm kinh nghiệm',
+      content:
+        'Thêm kinh nghiệm làm việc để giúp nhà tuyển dụng hiểu hơn về bạn',
+      buttonAction: (
+        <ButtonAction
+          title={<PlusOutlined className="text-[#691f74] cursor-pointer" />}
+          onClick={handleClick}
+        />
+      ),
+    },
+    {
+      header: {
+        title: 'Ngoại ngữ',
+        suffixIcon: <Language width={20} height={20} className="font-bold" />,
+      },
+      buttonTitle: 'Thêm ngoại ngữ',
+      content:
+        'Bạn biết những ngoại ngữ nào? Hãy thêm vào để tăng độ "hot" cho hồ sơ nhé.',
+      buttonAction: (
+        <ButtonAction
+          title={<PlusOutlined className="text-[#691f74] cursor-pointer" />}
+          onClick={handleClick}
+        />
+      ),
+    },
+    {
+      header: {
+        title: 'Kỹ năng / Công cụ',
+        suffixIcon: (
+          <PencilSkill width={20} height={20} className="font-bold" />
+        ),
+      },
+      buttonTitle: 'Thêm kỹ năng',
+      content:
+        'Kỹ năng / công cụ giúp bạn nổi bật hơn trong mắt nhà tuyển dụng.',
+      buttonAction: (
+        <ButtonAction
+          title={<PlusOutlined className="text-[#691f74] cursor-pointer" />}
+          onClick={handleClick}
+        />
+      ),
+    },
+  ];
+
   return (
-    <div>
-      <div className="flex justify-between">
-        <Title level={4}>Thành tích / Kỹ năng nổi bật</Title>
-        <Tooltip title="Cập nhật">
-          <ButtonAction
-            title={<EditOutlined className="text-[#691f74] cursor-pointer" />}
-          />
-        </Tooltip>
-      </div>
-    </div>
+    <>
+      {items.map((item, index) => (
+        <>
+          <ProfileSection key={index} {...item} onClick={handleClick} />
+          {index !== items.length - 1 && <Divider />}
+        </>
+      ))}
+    </>
   );
 };
 
