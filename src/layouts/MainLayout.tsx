54e5c4dfb53d2c2b1909c1cf1519595fb1963779
PanDaIT03 <daiphucduongvinh203@gmail.com>
2024-09-25 11:24:12 +0700

    [FIX] add default items to Header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,29 +1,13 @@
 import Layout from 'antd/es/layout/layout';
 import { ReactNode } from 'react';
 
-import PATH from '~/utils/path';
 import Footer from './Footer/Footer';
-import Header, { MenuItem } from './Header/Header';
-
-const items: MenuItem[] = [
-  {
-    key: PATH.JOB_SEEKER,
-    label: 'Danh sách ứng viên',
-  },
-  {
-    key: PATH.JOB_LIST,
-    label: 'Tin tuyển dụng',
-  },
-  {
-    key: 'blog',
-    label: 'Blog',
-  },
-];
+import Header from './Header/Header';
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <Layout className="min-h-screen">
-      <Header items={items} />
+      <Header />
       <div className="bg-main">{children}</div>
       <Footer />
     </Layout>
