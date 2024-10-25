c02e40d24cfdff67f68057ccf19c02a9fb5c9f17
PanDaIT03 <daiphucduongvinh203@gmail.com>
2024-10-25 16:27:38 +0700

    [FIX] fix getMe in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { useEffect, useRef } from 'react';
 
 import { MessageProvider } from './contexts/MessageProvider';
+import { useAppDispatch } from './hooks/useStore';
 import AppRouter from './routes/AppRouter';
+import { getMe } from './store/thunk/auth';
 import toast from './utils/functions/toast';
 
 function App() {
+  const flagRef = useRef(false);
+  const dispatch = useAppDispatch();
+
+  const token = localStorage.getItem('token1');
   const clientId = import.meta.env.VITE_APP_CLIENT_ID;
 
   if (!clientId) {
     toast.error('Google Client ID is missing!');
     return <div>Error: Google Client ID is not set.</div>;
   }
+
+  useEffect(() => {
+    if (!token) return;
+
+    if (!flagRef.current) {
+      dispatch(getMe());
+      flagRef.current = true;
+    }
+  }, [token]);
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
