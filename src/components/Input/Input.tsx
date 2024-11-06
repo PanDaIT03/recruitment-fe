e99cf6b319d43ed5ef59f8c9c5b44376209bc78e
PanDaIT03 <daiphucduongvinh203@gmail.com>
2024-11-06 10:14:43 +0700

    [FIX] increase the distance between prefix and content

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,7 @@
 import { ConfigProvider, Input as InputAntd, InputProps } from 'antd';
 import classNames from 'classnames';
 import { memo } from 'react';
+import './Input.scss';
 
 const Input = ({ className, ...props }: InputProps) => {
   const { allowClear = false } = props;
