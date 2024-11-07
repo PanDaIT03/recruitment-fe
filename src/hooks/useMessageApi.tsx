16541e0072de9a0fa9d1d2f464a800536820af44
PanDaIT03 <daiphucduongvinh203@gmail.com>
2024-11-07 18:21:28 +0700

    [FIX] fix params apiFn in useMessageApi hook

--- src/hooks/useMessageApi.tsx
+++ src/hooks/useMessageApi.tsx
@@ -1,36 +1,45 @@
 import { useCallback, useState } from 'react';
 import { useMessage } from '~/contexts/MessageProvider';
 
-interface IProps {
-  apiFn: (params?: any) => Promise<IBaseResponse | any>;
+interface IProps<T> {
+  showProcessMessage?: boolean;
+  apiFn: (params: T) => Promise<IBaseResponse | any>;
   onSuccess?: (response: any) => void;
   onError?: (error: any) => void;
 }
 
-const useMessageApi = ({ apiFn, onSuccess, onError }: IProps) => {
+const useMessageApi = <T,>({
+  showProcessMessage = true,
+  apiFn,
+  onSuccess,
+  onError,
+}: IProps<T>) => {
   const { messageApi } = useMessage();
   const [isPending, setIsPending] = useState(false);
 
-  const mutate = useCallback(async (params?: any) => {
+  const mutate = useCallback(async (params: T) => {
     setIsPending(true);
 
     try {
-      messageApi.loading({ key: 'messageApi', content: 'Đang thực hiện...' });
+      showProcessMessage &&
+        messageApi.loading({ key: 'messageApi', content: 'Đang thực hiện...' });
 
       const response = await apiFn(params);
       const { message, statusCode } = response;
 
       if (statusCode === 200) {
-        messageApi.success({ key: 'messageApi', content: message });
+        showProcessMessage &&
+          messageApi.success({ key: 'messageApi', content: message });
         onSuccess && onSuccess(response);
       }
 
       setIsPending(false);
     } catch (error: any) {
-      messageApi.error({
-        key: 'messageApi',
-        content: `Có lỗi xảy ra: ${error?.response?.data?.message || 'Không xác định'}`,
-      });
+      showProcessMessage &&
+        messageApi.error({
+          key: 'messageApi',
+          content: `Có lỗi xảy ra: ${error?.response?.data?.message || 'Không xác định'}`,
+        });
 
       setIsPending(false);
       onError && onError(error);
