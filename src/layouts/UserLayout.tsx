e54349ea5b2c358e730941a53222e51ef78eefbb
PanDaIT03 <daiphucduongvinh203@gmail.com>
2024-11-07 09:50:48 +0700

    [FEAT] reponsive Menu & UserLayout, add HeaderMenu

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -6,7 +6,6 @@ import {
   Image as ImageAntd,
   Layout,
   message,
-  Upload,
   UploadFile,
   UploadProps,
 } from 'antd';
@@ -37,6 +36,7 @@ import {
 } from '~/assets/svg';
 import Button from '~/components/Button/Button';
 import ButtonAction from '~/components/Button/ButtonAction';
+import Dragger from '~/components/Dragger/Dragger';
 import FormItem from '~/components/Form/FormItem';
 import FormWrapper from '~/components/Form/FormWrapper';
 import Input from '~/components/Input/Input';
@@ -48,7 +48,6 @@ import { defaultCoverImage } from '~/mocks/data';
 import icons from '~/utils/icons';
 import PATH from '~/utils/path';
 import Header from './Header/Header';
-import Dragger from '~/components/Dragger/Dragger';
 
 interface IChildren {
   title: string;
@@ -111,66 +110,59 @@ const Sider = ({ setIsOpenInfoModal, setIsOpenAvatarModal }: ISiderProps) => {
   const { currentUser } = useAppSelector((state) => state.auth);
 
   return (
-    <Flex vertical className="lg:col-span-3" gap={16}>
-      <div className="overflow-hidden rounded-xl bg-white shadow-card p-0 shadow">
-        <div className="w-full">
-          <div className="h-[9rem] bg-[url('assets/img/profile_cover_image.jpg')] bg-cover bg-center bg-no-repeat"></div>
-          <div className="-mt-[59px] px-6 pb-6">
-            <Flex align="end" justify="space-between" gap={16}>
-              <div className="relative w-fit rounded-full border-[3px] border-white">
-                <div className="rounded-full bg-white w-[108px] h-[108px]">
-                  <div className="inline-block aspect-square h-full w-full rounded-full border border-gray-200 overflow-hidden">
-                    <ImageAntd
-                      width={108}
-                      height={108}
-                      src={defaultCoverImage}
-                    />
-                  </div>
+    // <Flex className="w-full lg:col-span-3">
+    <div className="w-full h-max overflow-hidden rounded-xl bg-white shadow-card p-0 shadow lg:col-span-3">
+      <div className="w-full">
+        <div className="h-[9rem] bg-[url('assets/img/profile_cover_image.jpg')] bg-cover bg-center bg-no-repeat"></div>
+        <div className="-mt-[59px] px-6 pb-6">
+          <Flex align="end" justify="space-between" gap={16}>
+            <div className="relative w-fit rounded-full border-[3px] border-white">
+              <div className="rounded-full bg-white w-[108px] h-[108px]">
+                <div className="inline-block aspect-square h-full w-full rounded-full border border-gray-200 overflow-hidden">
+                  <ImageAntd width={108} height={108} src={defaultCoverImage} />
                 </div>
-                <ButtonAction
-                  tooltipTitle="Sửa"
-                  title={<EditOutlined className="text-white cursor-pointer" />}
-                  className="bg-[#691f74] py-[2px] px-[6px] absolute bottom-0 right-0 hover:!bg-[#461a53]"
-                  onClick={() => setIsOpenAvatarModal(true)}
-                />
               </div>
-              <Button
-                title="Sửa"
-                borderType="dashed"
-                iconBefore={<EditOutlined />}
-                onClick={() => setIsOpenInfoModal(true)}
+              <ButtonAction
+                tooltipTitle="Sửa"
+                title={<EditOutlined className="text-white cursor-pointer" />}
+                className="bg-[#691f74] py-[2px] px-[6px] absolute bottom-0 right-0 hover:!bg-[#461a53]"
+                onClick={() => setIsOpenAvatarModal(true)}
               />
-            </Flex>
-            <div className="mt-4">
-              <p className="text-lg font-semibold">{currentUser.fullName}</p>
-              <p className="text-sm text-sub font-normal">
-                {currentUser.email}
-              </p>
             </div>
+            <Button
+              title="Sửa"
+              borderType="dashed"
+              iconBefore={<EditOutlined />}
+              onClick={() => setIsOpenInfoModal(true)}
+            />
+          </Flex>
+          <div className="mt-4">
+            <p className="text-lg font-semibold">{currentUser.fullName}</p>
+            <p className="text-sm text-sub font-normal">{currentUser.email}</p>
           </div>
-          <div className="hidden lg:block">
-            <Divider dashed className="!m-0" />
-            <div className="flex flex-col gap-4 mt-3 px-6 py-6">
-              {items.map((item, index) => (
-                <div key={index}>
-                  <h3 className="font-bold">{item.title}</h3>
-                  {item.children.map((child, index) => (
-                    <Link
-                      key={index}
-                      to={child.href}
-                      className="flex p-2 items-center gap-2 rounded-md cursor-pointer hover:bg-[#f5f5f5] hover:text-[#000000E0]"
-                    >
-                      <span>{child.icon}</span>
-                      <span>{child.title}</span>
-                    </Link>
-                  ))}
-                </div>
-              ))}
-            </div>
+        </div>
+        <div className="hidden lg:block">
+          <Divider dashed className="!m-0" />
+          <div className="flex flex-col gap-4 mt-3 px-6 py-6">
+            {items.map((item, index) => (
+              <div key={index}>
+                <h3 className="font-bold">{item.title}</h3>
+                {item.children.map((child, childIndex) => (
+                  <Link
+                    key={childIndex}
+                    to={child.href}
+                    className="flex p-2 items-center gap-2 rounded-md cursor-pointer hover:bg-[#f5f5f5] hover:text-[#000000E0]"
+                  >
+                    <span>{child.icon}</span>
+                    <span>{child.title}</span>
+                  </Link>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </Flex>
+    </div>
   );
 };
 
@@ -310,9 +302,9 @@ const UserLayout = () => {
   }, [placements]);
 
   return (
-    <Layout>
+    <Layout className="min-h-screen">
       <Header />
-      <div className="w-full py-4 px-8 mx-auto max-w-7xl min-h-screen grid grid-cols-1 lg:grid-cols-10 gap-4">
+      <div className="w-full py-4 px-8 mx-auto max-w-7xl grid grid-cols-1 lg:grid-cols-10 gap-4">
         <Sider
           setIsOpenInfoModal={setIsOpenInfoModal}
           setIsOpenAvatarModal={setIsOpenAvatarModal}
@@ -333,7 +325,11 @@ const UserLayout = () => {
           {formItems.map((formItem) => {
             const { item, ...others } = formItem;
 
-            return <FormItem {...others}>{item}</FormItem>;
+            return (
+              <FormItem key={formItem.name} {...others}>
+                {item}
+              </FormItem>
+            );
           })}
         </FormWrapper>
       </Modal>
