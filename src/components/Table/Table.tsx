0507df8101fa0f921c2ce0da7cc8e7aa7ff0c3e4
phucducz <phucducduongvinh05102003@gmail.com>
2025-01-03 15:56:55 +0700

    fix: update Table css

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,40 +1,29 @@
-import {
-  ConfigProvider,
-  Empty,
-  Table as TableAntD,
-  TablePaginationConfig,
-  TableProps,
-} from 'antd';
+import { Table as TableAntD, TablePaginationConfig, TableProps } from 'antd';
 import { ColumnsType } from 'antd/es/table';
 import classNames from 'classnames';
 
-import { NoData } from '~/assets/svg';
-import './index.scss';
+// import './index.scss';
 
 interface IBaseTWithID {
   id: number | string;
 }
 
-const CustomerEmptyData = () => {
-  return (
-    <Empty
-      description={<span className="text-admin-primary">Không có dữ liệu</span>}
-      image={<NoData />}
-      imageStyle={{ height: '70px' }}
-    />
-  );
-};
+// const CustomerEmptyData = () => {
+//   return (
+//     <Empty
+//       description={<span className="text-admin-primary">Không có dữ liệu</span>}
+//       image={<NoData />}
+//       imageStyle={{ height: '70px' }}
+//     />
+//   );
+// };
 
 const Table = <T extends IBaseTWithID>({
-  dataSource,
   columns,
-  loading,
   pagination,
-  className,
   ...passProps
 }: TableProps<T>) => {
   const paginationParams = pagination as TablePaginationConfig;
-  const tableClasses = classNames('admin-custom-table', className);
 
   const formattedColumns = columns?.map((col) => ({
     ...col,
@@ -52,52 +41,51 @@ const Table = <T extends IBaseTWithID>({
   })) as ColumnsType<any>;
 
   return (
-    <ConfigProvider
-      theme={{
-        // token: {
-        // colorText: 'white',
-        // colorBorder: '#ffac69',
-        // colorBgTextHover: '#ffac69',
-        // colorBgContainer: '#2f2f41b3',
-        // colorIcon: 'white',
-        // colorTextDisabled: '#d3d3d3',
-        // colorIconHover: '#ffac69',
-        // },
-        components: {
-          Table: {
-            colorText: '#ff0000',
-            colorIcon: '#ff0000',
-          },
-        },
+    // <ConfigProvider
+    //   theme={
+    //     {
+    //       // token: {
+    //       // colorText: 'white',
+    //       // colorBorder: '#ffac69',
+    //       // colorBgTextHover: '#ffac69',
+    //       // colorBgContainer: '#2f2f41b3',
+    //       // colorIcon: 'white',
+    //       // colorTextDisabled: '#d3d3d3',
+    //       // colorIconHover: '#ffac69',
+    //       // },
+    //       // components: {
+    //       //   Table: {
+    //       //     colorText: '#ff0000',
+    //       //     colorIcon: '#ff0000',
+    //       //   },
+    //       // },
+    //     }
+    //   }
+    //   renderEmpty={CustomerEmptyData}
+    // >
+    // </ConfigProvider>
+    <TableAntD
+      size="middle"
+      columns={formattedColumns}
+      scroll={{ x: 'max-content' }}
+      rowKey={(record) => record?.id ?? 'id'}
+      rowClassName={(_, index) => (index % 2 !== 0 ? 'even-row' : '')}
+      pagination={{
+        current: paginationParams?.current ?? 1,
+        pageSize: paginationParams?.pageSize ?? 10,
+        ...(paginationParams?.onChange && {
+          onChange: paginationParams.onChange,
+        }),
+        pageSizeOptions: [1, 10, 20],
+        showSizeChanger: true,
+        className: classNames(
+          '!mb-0 !mt-5 [&>li]:!mr-[8px]',
+          paginationParams?.className
+        ),
+        ...paginationParams,
       }}
-      renderEmpty={CustomerEmptyData}
-    >
-      <TableAntD
-        size="middle"
-        loading={loading}
-        dataSource={dataSource}
-        className={tableClasses}
-        columns={formattedColumns}
-        scroll={{ x: 'max-content' }}
-        rowKey={(record) => record?.id ?? 'id'}
-        rowClassName={(_, index) => (index % 2 !== 0 ? 'even-row' : '')}
-        {...passProps}
-        pagination={{
-          current: paginationParams?.current ?? 1,
-          pageSize: paginationParams?.pageSize ?? 10,
-          ...(paginationParams?.onChange && {
-            onChange: paginationParams.onChange,
-          }),
-          pageSizeOptions: [1, 10, 20],
-          showSizeChanger: true,
-          className: classNames(
-            '!mb-0 !mt-5 [&>li]:!mr-[8px]',
-            paginationParams?.className
-          ),
-          ...paginationParams,
-        }}
-      />
-    </ConfigProvider>
+      {...passProps}
+    />
   );
 };
 
