9047b8890ca8849b091d55f1bafd9a1926802857
trantuankiet11884 <trantuankiet11884@gmail.com>
2025-01-06 16:25:50 +0700

    fix: const  status job

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -11,9 +11,9 @@ export const colSpan = 8;
 export const defaultImgUrl = mockFileList[0].url;
 
 export enum JOB_STATUS {
-  ACTIVE = 5,
-  INACTIVE = 6,
-  DELETED = 7,
+  ACTIVE = 6,
+  INACTIVE = 7,
+  DELETED = 8,
 }
 
 export const SELECT_PROPS = {
