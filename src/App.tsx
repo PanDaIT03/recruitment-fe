7fb79ef80352cdc9eeb382acd9b7c743897312f1
phucducz <phucducduongvinh05102003@gmail.com>
2025-02-19 12:03:19 +0700

    fix: remove getAllRoles in App comp

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import { useAppDispatch } from './hooks/useStore';
 import useToken from './hooks/useToken';
 import AppRouter from './routes/AppRouter';
 import { getMe } from './store/thunk/auth';
-import { getAllRoles } from './store/thunk/role';
 import toast from './utils/functions/toast';
 
 function App() {
@@ -23,9 +22,7 @@ function App() {
 
   useEffect(() => {
     if (!flagRef.current) {
-      dispatch(getAllRoles({}));
       if (token) dispatch(getMe());
-
       flagRef.current = true;
     }
   }, [token]);
