7d22a602cc0b175a26468331bdece2abace7dc3e
PanDaIT03 <daiphucduongvinh203@gmail.com>
2025-04-25 22:00:38 +0700

    feat: implement CRUD operations for menu view

--- src/apis/menuView/index.ts
+++ src/apis/menuView/index.ts
@@ -10,10 +10,38 @@ export interface IGetAllMenuView {
   createdDate?: string;
 }
 
+export interface ICreateMenuView {
+  title: string;
+  path: string;
+  orderIndex: number;
+  icon?: string;
+  iconType: string;
+  functionalIds: number[];
+}
+
+export type IUpdateMenuView = Partial<ICreateMenuView> & { id: number };
+
 export const MenuViewAPI = {
   getAllMenuViews: async (params: IGetAllMenuView) => {
     return await axiosApi.get('/menu-view/all', {
       params,
     });
+  },
+  uploadIcon: async (params: FormData) => {
+    return await axiosApi.post('/cloudinary/upload/icon', params, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
+  createMenuView: async (params: ICreateMenuView) => {
+    return await axiosApi.post('/menu-view', params);
+  },
+  updateMenuView: async (params: IUpdateMenuView) => {
+    const { id, ...rest } = params;
+    return await axiosApi.patch(`/menu-view/${id}`, rest);
+  },
+  deleteMenuView: async (id: number) => {
+    return await axiosApi.delete(`/menu-view/${id}`);
   },
 };
